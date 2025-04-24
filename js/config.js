2a4c73ba2944a5bb6ede795fa9e78decafe99b8f
geethika-0607 <geethika.singu@utdallas.edu>
2025-04-23 22:01:20 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_RPmSz3ljJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'jt0aejpjp2o4rga59ur54k61c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
